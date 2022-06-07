4eafb0a3467a1beb5c718d5ca4fd5de75ba85439
sevenflow <648813710@qq.com>
2022-06-07 22:37:30 +0800

    暂存

--- learn-demo/route/admin/custom.js
+++ learn-demo/route/admin/custom.js
@@ -52,5 +52,86 @@ module.exports = () => {
         })
     })
 
+    Router.delete('/', (req, res) => {
+        const {
+            ID
+        } = req.query;
+        
+        if(!ID) {
+            res.send({
+                data: '缺少必要参数'
+            }).end();
+            return false;
+        };
+
+        db.query(`SELECT * FROM custom_evaluation_table WHERE ID=${ID}`, (err, data) => {
+            if(err) {
+                res.status(500).send({
+                    data: '数据库报错'
+                }).end();
+                return;
+            };
+            if(!data.length) {
+                res.status(404).send({
+                    data: '没有当前数据'
+                }).end();
+                return;
+            };
+            db.query(`DELETE FROM custom_evaluation_table WHERE ID=${ID}`, (err, data) => {
+                if(err) {
+                    res.status(500).send({
+                        data: '数据库报错'
+                    }).end();
+                    return;
+                };
+                res.send({
+                    data: '删除成功'
+                }).end();
+            })
+        })
+    })
+
+    Router.post('/set.json', (req, res) => {
+        const {
+            ID,
+            title,
+            descrption,
+            src
+        } = req.body;
+        
+        if(!ID) {
+            res.send({
+                data: '缺少必要参数'
+            }).end();
+            return false;
+        };
+
+        db.query(`SELECT * FROM custom_evaluation_table WHERE ID=${ID}`, (err, data) => {
+            if(err) {
+                res.status(500).send({
+                    data: '数据库报错'
+                }).end();
+                return;
+            };
+            if(!data.length) {
+                res.status(404).send({
+                    data: '没有当前数据'
+                }).end();
+                return;
+            };
+            db.query(`UPDATE custom_evaluation_table SET title='${title}', descrption='${descrption}', src='${src}' WHERE ID=${ID}`, (err, data) => {
+                if(err) {
+                    res.status(500).send({
+                        data: '数据库报错'
+                    }).end();
+                    return;
+                };
+                res.send({
+                    data: '修改成功'
+                }).end();
+            })
+        })
+    })
+
     return Router;
 }
